541a5b700152f2d81e17265a5cf592e664236b2d
Sambhav Mehta <sambhavmehta361@gmail.com>
2023-10-22 23:20:56 +0530

    Added :construction: navbar

--- client/src/components/MainNav.jsx
+++ client/src/components/MainNav.jsx
@@ -1,5 +1,23 @@
+import React from 'react'
+import classes from './MainNav.module.css'
 const MainNav = () => {
-	return <div>MainNav</div>;
+	return <nav className={classes.nav}>
+		<div className={`${classes.left} ${classes.common}`}>
+			    <a href="#">Home</a>
+			    <a href="#">About Us</a>
+			    <a href="#">Contact</a>
+		</div>
+		<div className={`${classes.mid} ${classes.common}`}>
+			<a href="#">
+			<div className={classes.logo}>
+				{/* <img src=""/> */}
+			</div>
+			<h2 >EduTrack</h2>
+			</a>
+		</div>
+		<div className={`${classes.right} ${classes.common}`}>
+			<button>Sign In</button>
+		</div>
+	</nav>
 };
-
 export default MainNav;
