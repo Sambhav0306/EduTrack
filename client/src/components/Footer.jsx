cd66e00e20454aff23ab415415c382a7fbddaad3
Shaurya <vshaurya02@gmail.com>
2023-10-30 23:37:48 +0530

    Updated :lipstick: Contact page and Footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -7,64 +7,65 @@ import ghd from "../assets/github-ico-dark.svg";
 import insta from "../assets/instagram-ico.svg";
 import ln from "../assets/linkedin-ico.svg";
 const Footer = () => {
-	return (
-		<div className={classes.footer}>
-			<div className={classes.links}>
-				<div className={classes["links-div"]}>
-					<h3>Menu</h3>
-					<Link to="/about">
-						<p>About</p>
-					</Link>
-					<Link to="#">
-						<p>Courses</p>
-					</Link>
-					<Link to="/contact">
-						<p>Contact</p>
-					</Link>
-				</div>
-				<div className={classes["links-div"]}>
-					<h3>Resources</h3>
-					<a href="#" target="_blank">
-						<p>DSA</p>
-					</a>
-					<a href="#" target="_blank">
-						<p>Blogs</p>
-					</a>
-					<a href="#" target="_blank">
-						<p>ETC</p>
-					</a>
-				</div>
-				<div className={classes["links-div"]}>
-					<h3>Follow Us</h3>
-					<div className={classes["socialmedia"]}>
-						<a href="#" target="_blank">
-							<img src={fb} alt="" />
-						</a>
-						<a href="#" target="_blank">
-							<img src={tt} alt="" />
-						</a>
-						<a href="#" target="_blank">
-							<img src={ln} alt="" />
-						</a>
-						<a href="#" target="_blank">
-							<img src={ghd} alt="" />
-						</a>
-						<a href="#" target="_blank">
-							<img src={insta} alt="" />
-						</a>
-					</div>
-				</div>
-				<div className={classes.divider} />
-				<div className={classes["links-div"]}>
-					<h1>EduTrack </h1>
-				</div>
-				<hr />
-			</div>
-			<div className={classes.copyright}>
-				<div>@{new Date().getFullYear()} EduTrack || All right reserved.</div>
-			</div>
-		</div>
-	);
+  return (
+    <div className={classes.footer}>
+      <div className={classes.links}>
+        <div className={classes["links-div"]}>
+          <h3>Menu</h3>
+          <a href="/about">
+            <p>About</p>
+          </a>
+          <a href="/courses">
+            <p>Courses</p>
+          </a>
+          <a href="/contact">
+            <p>Contact</p>
+          </a>
+        </div>
+        <div className={classes["links-div"]}>
+          <h3>Resources</h3>
+          <a href="#" target="_blank">
+            <p>DSA</p>
+          </a>
+          <a href="#" target="_blank">
+            <p>Blogs</p>
+          </a>
+          <a href="#" target="_blank">
+            <p>ETC</p>
+          </a>
+        </div>
+        <div className={classes["links-div"]}>
+          <h3>Follow Us</h3>
+          <div className={classes["socialmedia"]}>
+            <a href="#" target="_blank">
+              <img src={fb} alt="" />
+            </a>
+            <a href="#" target="_blank">
+              <img src={tt} alt="" />
+            </a>
+            <a href="#" target="_blank">
+              <img src={ln} alt="" />
+            </a>
+            <a href="#" target="_blank">
+              <img src={ghd} alt="" />
+            </a>
+            <a href="#" target="_blank">
+              <img src={insta} alt="" />
+            </a>
+          </div>
+        </div>
+        <div className={classes.divider} />
+        <a href="/" className={classes["links-div-logo"]}>
+          <div className={classes["logo"]}>{/* <img src=""/> */}</div>
+          <h1>EduTrack</h1>
+        </a>
+        <hr />
+      </div>
+      <div className={classes.copyright}>
+        <div>@{new Date().getFullYear()} EduTrack || All right reserved.</div>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
